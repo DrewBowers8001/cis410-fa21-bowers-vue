f5745607967f7d8c0400090fdd95ecbf01120b81
unknown <drew06@colostate.edu>
2021-12-13 22:02:40 -0700

    added to home page

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-      <h1> Scheduling </h1>
-      <h3> Welcome to your Employee Schedules</h3>  
+      <h1><strong> Scheduling</strong> </h1>
+      <h3><strong> Welcome to your Employee Schedules</strong></h3>  
 
 
       <br/> 
-     <img src="@/assets/photos/pexels-andrea-piacquadio-927022.jpg" width="700" height="500">
-
+     <img src="@/assets/photos/pexels-andrea-piacquadio-927022.jpg" width="700" height="500" padding="10px">
+      <br/> 
+      <br/> 
+      <div>
+     <h4> This app will allow you to look at your scheduled work times. You will also be able to view others availibility as well as submit your own availibility in order to cover shifts or get shifts covered</h4>
+</div>
       
 
 </div>
